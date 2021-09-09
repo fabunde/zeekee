4fdc8fae05e9414cc134b4e49716184c28108b6b
fabunde <fabunde15@gmail.com>
2021-09-09 01:35:14 -0400

    experiment with 3d animation in web; has error about hook call

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import RaceTrack from './components/RaceTrack';
+import Render3Dobject from './components/3D/Rende3Dobject';
 
 function App() {
   return (
     <div className="App">
       <RaceTrack/>
+      {/* <Render3Dobject/> */}
     </div>
   );
 }
